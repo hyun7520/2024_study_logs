c916cdd15ac81084a2d0802b26f107c0e59e35f3
hyun7520 <sanghyun123452@gmail.com>
2024-08-27 20:49:55 +0900

    ADD: 알고리즘 공부 내역

--- main/src/CodeTest/프로그래머스/N진수게임.java
+++ main/src/CodeTest/프로그래머스/N진수게임.java
@@ -4,16 +4,26 @@ public class N진수게임 {
 
     public static void main(String[] args) {
 
+        N진수게임Solution sol = new N진수게임Solution();
 
+        System.out.println(sol.solution(2, 4, 2, 1));
     }
 }
 
 class N진수게임Solution {
     public String solution(int n, int t, int m, int p) {
-        String answer = "";
 
+        StringBuilder convert = new StringBuilder();
+        StringBuilder answer = new StringBuilder();
 
+        for(int i = 0; convert.length() < t * m; i++) {
+            convert.append(Integer.toString(i, n));
+        }
 
-        return answer;
+        for(int i = p - 1; answer.length() < t; i += m) {
+            answer.append(convert.charAt(i));
+        }
+
+        return answer.toString().toLowerCase();
     }
 }
