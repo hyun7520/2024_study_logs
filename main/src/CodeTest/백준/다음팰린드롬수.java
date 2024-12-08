b7eca836006ca54e20687bd581463bfac8a2ca59
hyun7520 <sanghyun123452@gmail.com>
2024-12-08 21:26:27 +0900

    ADD: 알고리즘 공부 내역

--- main/src/CodeTest/백준/다음팰린드롬수.java
+++ main/src/CodeTest/백준/다음팰린드롬수.java
@@ -1,6 +1,5 @@
 package CodeTest.백준;
 
-
 import java.io.*;
 
 public class 다음팰린드롬수 {
@@ -14,11 +13,50 @@ public class 다음팰린드롬수 {
         int length = input.length();
         boolean isOdd = (length % 2 == 1);
 
-        String palindrome = "";
+        String left = input.substring(0, (length + 1) / 2);
 
-        if(isOdd) {
-
+        String palindrome = checkPalindrome(left, isOdd);
+        if(palindrome.compareTo(input) > 0) {
+            return palindrome;
         }
 
+        left = incrementNum(left);
+        if (left.length() > (length + 1) / 2) {
+            return "1" + "0".repeat(length - 1) + "1";
+        }
+        return checkPalindrome(left, isOdd);
+    }
+
+    public static String checkPalindrome(String input, boolean isOdd) {
+
+        StringBuilder right = new StringBuilder(input);
+
+        if(isOdd) {
+            right.deleteCharAt(right.length() - 1);
+        }
+
+        return input + right.reverse();
+    }
+
+    public static String incrementNum(String input) {
+
+        StringBuilder sb = new StringBuilder(input);
+        int carry = 1;
+
+        for(int i = sb.length() - 1; i >= 0; i--) {
+            int curDigit = sb.charAt(i) - '0' + carry;
+            if(curDigit == 10) {
+                sb.setCharAt(i, '0');
+            } else {
+                sb.setCharAt(i, (char)(curDigit + '0'));
+                carry = 0;
+                break;
+            }
+        }
+
+        if(carry == 1) {
+            sb.insert(0, '1');
+        }
+        return sb.toString();
     }
 }
